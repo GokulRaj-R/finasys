2c87793de26f36bdf01039b5896b423b127e35fb
mrpriyansh <mr_priyanshgaharana@gmail.com>
2020-10-20 14:25:12 +0530

    contracts working fine

---
+++ ethereum/document.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+import DocumentSOL from './build/Document.solDocument.json';
+const document = (address) => {
+    return new web3.eth.Contract(
+        JSON.parse(DocumentSOL.interface), address
+    )
+}
+
+export default document;
---
+++ ethereum/loanFactory.js
@@ -0,0 +1,10 @@
+import web3 from './web3';
+import loanFactorySOL from './build/Loan.solLoanFactory.json';
+
+const loanFactory = () => {
+    return new web3.eth.Contract(
+        JSON.parse(loanFactorySOL.interface), '0xC70B0F886fD43f1FD39061F6c7A5f99783d274E6'
+    )
+}
+
+export default loanFactory;
